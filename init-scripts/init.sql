fc4fb4bcd7f01c9449ea07e164520a035ce2e4d4
Flávio Rodolfo <rodolphoflavio@gmail.com>
2024-05-14 20:58:20 -0300

    Configuração inicial do banco de dados

---
+++ init-scripts/init.sql
@@ -0,0 +1,17 @@
+--CREATE DATABASE transfeera_db ;
+
+CREATE SCHEMA IF NOT EXISTS pagamento;
+
+CREATE TYPE pagamento.tipo_chave_pix_enum AS ENUM ('CPF', 'CNPJ', 'EMAIL', 'TELEFONE', 'CHAVE_ALEATORIA');
+
+
+CREATE TABLE pagamento.recebedores (
+	recebedor_id SERIAL PRIMARY KEY,
+	cpf_cnpj VARCHAR(20) NOT NULL,
+	nome VARCHAR(100) NOT NULL,
+	tipo_chave_pix pagamento.tipo_chave_pix_enum NOT NULL,
+	chave_pix VARCHAR(140) NOT NULL,
+	email varchar(250) DEFAULT NULL
+	
+	
+)
