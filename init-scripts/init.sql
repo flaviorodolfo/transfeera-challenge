d24036146a48de61fdee4f4aad8ab00dfa060971
Flávio Rodolfo <rodolphoflavio@gmail.com>
2024-05-15 19:28:04 -0300

    Adição de campo status na criação do DB

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -4,14 +4,16 @@ CREATE SCHEMA IF NOT EXISTS pagamento;
 
 CREATE TYPE pagamento.tipo_chave_pix_enum AS ENUM ('CPF', 'CNPJ', 'EMAIL', 'TELEFONE', 'CHAVE_ALEATORIA');
 
+CREATE TYPE pagamento.status_recebedor_enum AS ENUM('RASCUNHO','VALIDADO');
 
-CREATE TABLE pagamento.recebedores (
+CREATE TABLE recebedores (
 	recebedor_id SERIAL PRIMARY KEY,
 	cpf_cnpj VARCHAR(20) NOT NULL,
 	nome VARCHAR(100) NOT NULL,
 	tipo_chave_pix pagamento.tipo_chave_pix_enum NOT NULL,
 	chave_pix VARCHAR(140) NOT NULL,
-	email varchar(250) DEFAULT NULL
+	status_recebedor pagamento.status_recebedor_enum NOT NULL,
+	email VARCHAR(250) DEFAULT NULL
 	
 	
 )
