6f9aad8ee090710fc1b0d1ccd9e22278ca7ae3c1
Flávio Rodolfo <rodolphoflavio@gmail.com>
2024-05-18 17:18:35 -0300

    Melhoria na modelagem do BD

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -4,15 +4,13 @@ CREATE SCHEMA IF NOT EXISTS pagamento;
 
 CREATE TYPE pagamento.tipo_chave_pix_enum AS ENUM ('CPF', 'CNPJ', 'EMAIL', 'TELEFONE', 'CHAVE_ALEATORIA');
 
-CREATE TYPE pagamento.status_recebedor_enum AS ENUM('RASCUNHO','VALIDADO');
-
-CREATE TABLE recebedores (
+CREATE TABLE pagamento.recebedores (
 	recebedor_id SERIAL PRIMARY KEY,
 	cpf_cnpj VARCHAR(20) NOT NULL,
 	nome VARCHAR(100) NOT NULL,
 	tipo_chave_pix pagamento.tipo_chave_pix_enum NOT NULL,
 	chave_pix VARCHAR(140) NOT NULL,
-	status_recebedor pagamento.status_recebedor_enum NOT NULL,
+	status_recebedor VARCHAR(15) DEFAULT 'Rascunho',
 	email VARCHAR(250) DEFAULT NULL
 	
 	
